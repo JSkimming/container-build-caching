4f40b9b3f5c2467179d12eab7d2a749251253ce9
James Skimming <JSkimming@users.noreply.github.com>
2018-04-17 08:53:38 +0100

    :godmode: Don't fail if there is no base image

--- build.ps1
+++ build.ps1
@@ -6,26 +6,41 @@ function IsNotCiBuild {
     return [string]::IsNullOrWhiteSpace($Env:APPVEYOR)
 }
 
-function ExecuteCommand ([string] $command) {
+function ExecuteCommand ([string] $command, [bool] $throwOnError = $true) {
     Write-Host $command
     Invoke-Expression $command
     if ($LASTEXITCODE -ne 0) {
-        throw "An error occurred executing the command '$command'. LASTEXITCODE=$LASTEXITCODE"
+        if ($throwOnError){
+            throw "An error occurred executing the command '$command'. LASTEXITCODE=$LASTEXITCODE"
+        }
+
+        return $false
     }
+
+    return $true
 }
 
 function RunBuild ([string] $image) {
     # Download the cached images.
-    ExecuteCommand "docker pull appcyc.azurecr.io/cbc-$image-build:latest"
-    ExecuteCommand "docker pull appcyc.azurecr.io/cbc-$image`:latest"
+    $pulled = ExecuteCommand "docker pull appcyc.azurecr.io/cbc-$image-build:latest" $false
+    if ($pulled -ne $true) {
+        Write-Host "There is no latest build image 'appcyc.azurecr.io/cbc-$image-build:latest' using the base image."
+        ExecuteCommand "docker tag microsoft/aspnetcore-build:2.0.0 appcyc.azurecr.io/cbc-$image-build:latest"
+    }
+
+    $pulled = ExecuteCommand "docker pull appcyc.azurecr.io/cbc-$image`:latest" $false
+    if ($pulled -ne $true) {
+        Write-Host "There is no latest image 'appcyc.azurecr.io/cbc-$image`:latest' using the base image."
+        ExecuteCommand "docker tag microsoft/aspnetcore:2.0.0 appcyc.azurecr.io/cbc-$image`:latest"
+    }
 
     # Tag the image with the previous image
     ExecuteCommand "docker tag appcyc.azurecr.io/cbc-$image-build:latest cbc-$image-build:previous"
     ExecuteCommand "docker tag appcyc.azurecr.io/cbc-$image`:latest cbc-$image`:previous"
 
     # Run the build
-    ExecuteCommand "docker build --cache-from cbc-$image-build:previous -f ./build/Dockerfile.$image.build -t cbc-$image-build:latest -t appcyc.azurecr.io/cbc-$image-build:$buildversion -t appcyc.azurecr.io/cbc-$image-build:latest ."
-    ExecuteCommand "docker build --cache-from cbc-$image`:previous -f ./build/Dockerfile.$image.package -t cbc-$image`:latest -t appcyc.azurecr.io/cbc-$image`:$buildversion -t appcyc.azurecr.io/cbc-$image`:latest ."
+    ExecuteCommand "docker build --cache-from cbc-$image-build:previous -f ./build/$image.build.Dockerfile -t cbc-$image-build:latest -t appcyc.azurecr.io/cbc-$image-build:$buildversion -t appcyc.azurecr.io/cbc-$image-build:latest ."
+    ExecuteCommand "docker build --cache-from cbc-$image`:previous -f ./build/$image.package.Dockerfile -t cbc-$image`:latest -t appcyc.azurecr.io/cbc-$image`:$buildversion -t appcyc.azurecr.io/cbc-$image`:latest ."
 
     # Push to the repository
 
@@ -53,6 +68,11 @@ function RunBuild ([string] $image) {
         return $true
     }
 }
+
+# Pull the latest base images, these are used by all the builds and are also used as the previous image if one is not
+# present in the remote container repository.
+ExecuteCommand "docker pull microsoft/aspnetcore-build:2.0.0"
+ExecuteCommand "docker pull microsoft/aspnetcore:2.0.0"
 
 RunBuild "identity"
 
